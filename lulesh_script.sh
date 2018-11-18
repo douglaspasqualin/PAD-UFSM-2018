d6ba2f229ddfcc9d93ed19275c67d1c0281042f0
Douglas Pereira Pasqualin <douglas.pasqualin@gmail.com>
2018-11-17 23:33:28 -0200

    robust script

--- lulesh_script.sh
+++ lulesh_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#MPIRUN=mpirun
-MPIRUN=/home/dpasqualin/opt/openmpi-4.0.0/bin/mpirun
+MPIRUN=mpirun
+#MPIRUN=/home/dpasqualin/opt/openmpi-4.0.0/bin/mpirun
 
 #Processors  must be a cube of an integer (1, 8, 27, ...)
 #PS: The number of OpenMP threads will be used in each processor. 
@@ -17,59 +17,34 @@ CYCLES=1
 #delete outputfile
 rm -f output.txt
 
-##########OpenMP / MPI version
-echo "Executing OpenMP / MPI version - 8 thread"
-export OMP_NUM_THREADS=1
-for i in `seq 1 30`
+############OpenMP / MPI
+echo "Executing OpenMP / MPI version"
+LULESH_EXEC=LULESH-OpenMP_MPI/lulesh2.0
+THREADS=1
+export OMP_NUM_THREADS=$THREADS
+for i in `seq 1 180`
 do
-echo "executing $i ..."
-$MPIRUN -n $NUM_PROC LULESH-OpenMP_MPI/lulesh2.0  -s $CUBE_SIZE -i $CYCLES -q >> output.txt 
+  if [ `expr $i % 30` == 0 ]; then
+    echo "ok"
+    THREADS=$((THREADS + 1))
+    export OMP_NUM_THREADS=$THREADS
+  fi 
+  $MPIRUN -n $NUM_PROC $LULESH_EXEC  -s $CUBE_SIZE -i $CYCLES -q >> output.txt 
 done
-
-#16 
-echo "Executing OpenMP / MPI version - 16 thread"
-export OMP_NUM_THREADS=2
-for i in `seq 1 30`
-do
-$MPIRUN -n $NUM_PROC LULESH-OpenMP_MPI/lulesh2.0  -s $CUBE_SIZE -i $CYCLES -q >> output.txt 
-done
-
-#24
-echo "Executing OpenMP / MPI version - 24 thread"
-export OMP_NUM_THREADS=3
-for i in `seq 1 30`
-do
-$MPIRUN -n $NUM_PROC LULESH-OpenMP_MPI/lulesh2.0  -s $CUBE_SIZE -i $CYCLES -q >> output.txt 
-done
-
-
-#32
-echo "Executing OpenMP / MPI version - 32 thread"
-export OMP_NUM_THREADS=4
-for i in `seq 1 30`
-do
-$MPIRUN -n $NUM_PROC LULESH-OpenMP_MPI/lulesh2.0  -s $CUBE_SIZE -i $CYCLES -q >> output.txt 
-done
-
-
-#40
-echo "Executing OpenMP / MPI version - 40 thread"
-export OMP_NUM_THREADS=5
-for i in `seq 1 30`
-do
-$MPIRUN -n $NUM_PROC LULESH-OpenMP_MPI/lulesh2.0  -s $CUBE_SIZE -i $CYCLES -q >> output.txt 
-done
-
-
-#48
-echo "Executing OpenMP / MPI version - 48 thread"
-export OMP_NUM_THREADS=6
-for i in `seq 1 30`
-do
-$MPIRUN -n $NUM_PROC LULESH-OpenMP_MPI/lulesh2.0  -s $CUBE_SIZE -i $CYCLES -q >> output.txt 
-done
-
-
 
 
 ##########DASH version
+echo "Executing DASH version"
+LULESH_EXEC=LULESH-DASH/build/lulesh
+THREADS=1
+export OMP_NUM_THREADS=$THREADS
+for i in `seq 1 180`
+do
+  if [ `expr $i % 30` == 0 ]; then
+    echo "ok"
+    THREADS=$((THREADS + 1))
+    export OMP_NUM_THREADS=$THREADS
+  fi 
+  $MPIRUN -n $NUM_PROC $LULESH_EXEC  -s $CUBE_SIZE -i $CYCLES -px 2 -py 2 -pz 2 -q >> output.txt 
+done
+
