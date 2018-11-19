5f05c15f43db6055a43eaf35f57e95413fc6bee1
Douglas Pereira Pasqualin <douglas.pasqualin@gmail.com>
2018-11-18 23:29:02 -0200

    testing

--- lulesh_script.sh
+++ lulesh_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#MPIRUN=mpirun
-MPIRUN=/home/dpasqualin/opt/openmpi-4.0.0/bin/mpirun
+MPIRUN=mpirun
+#MPIRUN=/home/dpasqualin/opt/openmpi-4.0.0/bin/mpirun
 
 #Processors  must be a cube of an integer (1, 8, 27, ...)
 #PS: The number of OpenMP threads will be used in each processor. 
@@ -12,7 +12,7 @@ NUM_PROC=8
 CUBE_SIZE=30
 
 #Number of cycles to run
-CYCLES=1000
+CYCLES=1
 
 #delete outputfile
 rm -f output.txt
@@ -20,10 +20,8 @@ rm -f output.txt
 ############OpenMP / MPI
 echo "Executing OpenMP / MPI version"
 LULESH_EXEC=LULESH-OpenMP_MPI/lulesh2.0
-THREADS=1
-export OMP_NUM_THREADS=$THREADS
-echo "running  - $THREADS"
-for i in `seq 1 180`
+THREADS=0
+for i in `seq 0 180`
 do
   if [ `expr $i % 30` == 0 ] && [  "$i" -lt "180" ] ; then
     THREADS=$((THREADS + 1))
@@ -35,18 +33,18 @@ done
 
 
 ##########DASH version
-echo "Executing DASH version"
-LULESH_EXEC=LULESH-DASH/build/lulesh
-THREADS=1
-export OMP_NUM_THREADS=$THREADS
-echo "running  - $THREADS"
-for i in `seq 1 180`
-do
-  if [ `expr $i % 30` == 0 ] && [  "$i" -lt "180" ] ; then
-    echo "ok - $THREADS"
-    THREADS=$((THREADS + 1))
-    export OMP_NUM_THREADS=$THREADS
-  fi 
-  $MPIRUN -n $NUM_PROC $LULESH_EXEC  -s $CUBE_SIZE -i $CYCLES -px 2 -py 2 -pz 2 -q >> output.txt 
-done
+#echo "Executing DASH version"
+#LULESH_EXEC=LULESH-DASH/build/lulesh
+#THREADS=1
+#export OMP_NUM_THREADS=$THREADS
+#echo "running  - $THREADS"
+#for i in `seq 1 180`
+#do
+#  if [ `expr $i % 30` == 0 ] && [  "$i" -lt "180" ] ; then
+#    echo "ok - $THREADS"
+#    THREADS=$((THREADS + 1))
+#    export OMP_NUM_THREADS=$THREADS
+#  fi 
+#  $MPIRUN -n $NUM_PROC $LULESH_EXEC  -s $CUBE_SIZE -i $CYCLES -px 2 -py 2 -pz 2 -q >> output.txt 
+#done
 
